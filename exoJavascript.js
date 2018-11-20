1e09fc9390b009bd5e720d719a3110d1b1b8c3dc
Rémi <ponche62880@gmail.com>
2018-11-20 23:57:10 +0100

    le tableau est deja trié de base

---
+++ exoJavascript.js
@@ -0,0 +1,8 @@
+// declaration du tableau, fournis dans le cours 
+var students = ["Alexandre","Béatrice","Benoît","Émeric","Florian","Gwenaëlle","Jérémy","Johan","Justine M","Justine P","Justine T","Kévin","Loïc","Mathieu","Matthias","Rémi","Samuel","Sébastien","Sullivan","Thomas","Tiffany","Valentin","Yann"];
+
+let sortStudent = students.sort() ; 
+
+// je ne comprend pas l'idée de trier un tableau  déja trié 
+ 
+
