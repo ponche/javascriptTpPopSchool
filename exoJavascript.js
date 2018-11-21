8ee533911427a3166a518550e90e487aac87259b
Rémi <ponche62880@gmail.com>
2018-11-21 15:37:53 +0100

    first version damier

---
+++ exoJavascript.js
@@ -0,0 +1,56 @@
+// declaration des balise 
+let baliseButtonGenerator = document.querySelector("#buttonGenerator") ; 
+let baliseSuperDamier = document.querySelector("#superDamier") ; 
+
+// declaration du tableau en 2D 
+let damier = [] ; 
+
+
+// fonction creator cellule 
+function createCellule(couleur)
+{
+    let cellule = document.createElement("div") ; 
+    cellule.classList.add("cellule") ; 
+    if(couleur == "black")
+        cellule.classList.add("black") ; 
+    if(couleur == "white")
+        cellule.classList.add("white") ; 
+
+    return cellule 
+}
+
+function generatorDamier(row, column) 
+{
+    // on creer les row 
+    for(let i = 0 ; i < row; i++)
+    {
+        let divRow = document.createElement("div");
+        divRow.classList.add("row"); 
+        baliseSuperDamier.appendChild(divRow) ; 
+        damier.push(baliseSuperDamier) ; 
+        let rowEven = i % 2  ;
+
+        // on cree les div Cellule 
+        for(let j = 0 ; j < column; j++)
+        {
+            let divCellule = document.createElement("div") ; 
+            divCellule.classList.add("cellule"); 
+            // ajout de la couleur 
+            let columnEven = j % 2 ; 
+            if( rowEven == columnEven) 
+            {
+                divCellule.classList.add("black") ; 
+            }
+            else
+            {
+                divCellule.classList.add("white") ; 
+            }
+            divRow.appendChild(divCellule) ; 
+            // ajout dans le tableau 
+            // damier[i].push(divCellule) ; 
+
+
+        }
+        
+    }
+}
