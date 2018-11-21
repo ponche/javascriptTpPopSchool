ad9f6c3b4ee6e85eebe56ca2f13542984d393e11
Rémi <ponche62880@gmail.com>
2018-11-21 19:03:55 +0100

    premiere version horloge

---
+++ exoJavascript.js
@@ -0,0 +1,17 @@
+// recupération des balise HTML 
+let baliseBigBen = document.querySelector("#bigBen") ; 
+
+// declaration des variable 
+let bigBen = new Date ; 
+
+// code executer à chaque seconde 
+function ticTac()
+{
+    // se code sera executer tous les secondes 
+    console.log("tic tac") ; 
+    baliseBigBen.innerHTML = bigBen.getHours() + " : " + bigBen.getMinutes() ;  
+}
+
+// creation du timer 
+window.setInterval(ticTac, 100) ;  
+
