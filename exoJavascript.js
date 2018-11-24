32b6c611ca061b90fc306179f87329a8dc170013
Rémi <ponche62880@gmail.com>
2018-11-24 17:40:32 +0100

    add zero before unit

--- exoJavascript.js
+++ exoJavascript.js
@@ -10,7 +10,14 @@ function ticTac()
     // se code sera executer tous les secondes 
     bigBen = new Date ; 
     console.log("tic tac") ; 
-    baliseBigBen.innerHTML = bigBen.getHours() + " : " + bigBen.getMinutes() ;  
+    let hours = bigBen.getHours() ; 
+    let minutes = bigBen.getMinutes() ; 
+    let secondes = bigBen.getSeconds() ; 
+    // ajoute des zero si neccessaire 
+    let stringHours = hours < 10 ? "0" + hours : hours ; 
+    let stringMinutes = minutes < 10 ? "0" + minutes : minutes ;
+    let stringSecondes = secondes < 10 ? "0" + secondes : secondes ;  
+    baliseBigBen.innerHTML = stringHours + " : " + stringMinutes + "   secondes: " + stringSecondes ;  
 }
 
 // creation du timer 
