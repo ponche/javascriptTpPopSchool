e6e675cc15eb9715aefa9dbf657ffc9baea9ede9
Rémi <ponche62880@gmail.com>
2018-11-29 14:59:57 +0100

    first version vibrate

---
+++ exoJavascript.js
@@ -0,0 +1,9 @@
+// on met un timer régulier 
+window.setInterval(ticTac, 5000) ; 
+
+navigator.vibrate([500,100,250,100,500,500,100,250,100,500]) ; 
+
+function ticTac()
+{
+    navigator.vibrate([500,100,250,100,500,500,100,250,100,500]) ; 
+}
